2003fe5a90f71db3045366fbd3daeb9f32d5163b
MuzzammilM <muzzammilmallick568@gmail.com>
2023-10-10 10:55:13 +0400

    Exercise2.py

--- Chapter 1- Getting Started with Python/Exercises/practice5.py
+++ Chapter 1- Getting Started with Python/Exercises/practice5.py
@@ -1,2 +1,7 @@
 #Write four strings in different variables and print the output as one string (string concatenation)
-print (" my " + " name " + " is " + " muzzammil ")
+string1 = "My"
+string2 = "name"
+string3 = "is"
+string4 = "muzzammil"
+result = string1 + string2 + string3 + string4
+print (result)
--- Chapter 1- Getting Started with Python/Exercises/python6.py
+++ Chapter 1- Getting Started with Python/Exercises/python6.py
@@ -1,5 +1,18 @@
-f_name = "Muzzammil"
+'''f_name = "Muzzammil"
 l_name = "Mallick"
 name = f_name + l_name
 print (name,end="\t")
 print (" I am 18 years old ")
+name="muzzammil"
+print (f'My name is {name}.')
+age= 18
+print (f'the age is {age}.')
+course="Cyber security"
+print (f'I study {course}.')
+year=2005
+print (f'I was born in {year}.')'''
+
+import turtle
+turtle.forward(90)
+turtle.circle(100)
+turtle.color("red")
