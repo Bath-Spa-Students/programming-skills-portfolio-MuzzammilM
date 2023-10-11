fb504ad6f4de41222a33d1d9d84d455413834c3e
MuzzammilM <muzzammilmallick568@gmail.com>
2023-10-11 09:51:06 +0400

    exercise3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise4.py
@@ -0,0 +1,6 @@
+#Write three strings in different variables and print the output as one string.
+First_name= "Muhammed"
+Last_name= "Mallick"
+Middle_name= "Muzzammil"
+Full_name= f"{First_name} {Middle_name} {Last_name}"
+print (Full_name)
