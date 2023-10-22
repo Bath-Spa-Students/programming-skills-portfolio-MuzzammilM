7676507a2491ff378000866c7ccd3c12790dad5c
MuzzammilM <muzzammilmallick568@gmail.com>
2023-10-22 21:34:35 +0400

    chapter3 , exercises

---
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -0,0 +1,13 @@
+#Start with the list you used in Exercise 1, but instead of just printing each others name, print a message to them
+#The text of each message shhould be the same, but each message sould be personalized with the person's name.
+
+names = ['josh', 'gabriel', 'ethan']
+
+msg = f"Hello, {names[0].title()}!"
+print(msg)
+
+msg = f"Hello, {names[1].title()}!"
+print(msg)
+
+msg = f"Hello, {names[2].title()}!"
+print(msg)
---
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -0,0 +1,13 @@
+#If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite for dinner
+#Then use your list to print a message to each person, inviting them to dinner.
+
+guests = ['ali', 'joseph', 'shafeeq']
+
+name = guests[0].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[1].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[2].title()
+print(f"{name}, please come to dinner.")
